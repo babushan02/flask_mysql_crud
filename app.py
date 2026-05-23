55e6a9b4c9d52ffed4f4a69be76b263ad61dee77
Itz Babuz <babushan20060902@gmail.com>
2026-05-23 23:08:17 +0530

    fixed some errors in validation, Student GET all route completed, Student GET one route completed, Student DELETE route completed

--- app.py
+++ app.py
@@ -43,29 +43,29 @@ def create_student():
         data=request.get_json()
 
         if not data:
-            return jsonify({'error: NO data provided'}),400
+            return jsonify({'error': 'NO data provided'}),400
         if not data.get('full_name'):
-            return jsonify({'error: Full name id required'}),400
+            return jsonify({'error': 'Full name id required'}),400
         
         ##================Email test ===================##
 
         if not data.get('email'):
-            return jsonify({'error: Email is required'}),400
+            return jsonify({'error': 'Email is required'}),400
         existing_email= Student.query.filter_by(
             email=data['email']
         ).first()
         if existing_email:
-            return jsonify({'error: Email is already exists'}),400
+            return jsonify({'error': 'Email is already exists'}),400
         
         ##================Age test ===================##
 
         if not data.get('age'):
-            return jsonify({'error: Age is required'}),400
+            return jsonify({'error': 'Age is required'}),400
         if int(data['age'])<=0:
-            return jsonify({'error: age want to be positive integer'})
+            return jsonify({'error': 'age want to be positive integer'})
         
         if not data.get('joined_date'):
-            return jsonify({'error: joined date is required'}),400
+            return jsonify({'error': 'joined date is required'}),400
         student = Student(
             full_name=data['full_name'],
             email=data['email'],
@@ -81,7 +81,7 @@ def create_student():
         db.session.add(student)
         db.session.commit()
 
-        return jsonify({'message: Student created successfully'}),201
+        return jsonify({'message': 'Student created successfully'}),200
 
     except Exception as e:
         return jsonify({'error' : 'Internal server error',
@@ -89,13 +89,81 @@ def create_student():
     
 
        
-##---------------- GET students --------------------##   
+##--------------- GET (all) students -----------------##   
 
+app.route('/api/students', methods=['GET'])
+def get_students():
+    try:
+        students= Student.query.all()
+        student_list=[]
 
+        for student in students:
+            student_list.append({
+                'id': student.id,
+                'full_name': student.full_name,
+                'email': student.email,
+                'age': student.age,
+                'cgpa': student.cgpa,
+                'is_active': student.is_active,
+                'joined_date': student.joined_date.strftime('%Y-%m-%d'),
+                'created_at': student.created_at
+            })
+
+        return jsonify(student_list),200
+    except Exception as e:
+        return jsonify({'error':'Internal server error','details':str(e)})    
  
+##--------------- GET (one) student ------------------## 
 
+@app.route('/api/students/<int:id>', methods=['GET'])
+def get_student(id):
+    try:
+        student=Student.query.get(id)
+        if not student:
+            return jsonify({'error': 'Student not found'}),400
+        return jsonify({
+            'id': student.id,
+            'full_name': student.full_name,
+            'email': student.email,
+            'age': student.age,
+            'cgpa': student.cgpa,
+            'is_active': student.is_active,
+            'joined_date': student.joined_date.strftime('%Y-%m-%d'),
+            'created_at': student.created_at            
+        })
+    except Exception as e:
+        return jsonify({'error':'Internal server error','details':str(e)})
+    
+##-------------------- PUT student ---------------------## 
+
+# @app.route('/api/students/<int:id>', methods=['PUT'])
+# def update_student(id):
+
+##-------------------- DELETE student ------------------## 
+          
+@app.route('/api/students/<int:id>', methods=['DELETE'])
+def delete_student(id):
+
+    try:
+        student = Student.query.get(id)
+
+        if not student:
+            return jsonify({
+                'error': 'Student not found.'
+            }), 404
+
+        db.session.delete(student)
+        db.session.commit()
+
+        return jsonify({
+            'message': 'Student deleted successfully.'
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            'error': 'Internal server error.',
+            'details': str(e)
+        }), 500
        
-
-
 if __name__=='__main__':
     app.run(debug=True)
